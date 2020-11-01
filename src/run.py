03800d8328409b50ccb4737547e4e6d63fec040f
eran.saranga@gmail.com <eran.saranga@gmail.com>
2020-11-01 17:56:19 +0200

    more examples

--- src/run.py
+++ src/run.py
@@ -6,7 +6,7 @@ import os
 output_file_name="conv.gif"
 img = np.random.rand(7,7)
 kernel=np.random.rand(2,2)
-padding=0
+padding=1
 strides=1
 savelocation=os.path.join(pathlib.Path(__file__).parent.absolute(),output_file_name)
 
